775c2caa0ccd8f20d230117b07c807f56328631b
slorello89 <steve.lorello@redis.com>
2024-01-12 16:43:41 -0500

    updating default number of documents to upload

--- scripts/setup_beers.sh
+++ scripts/setup_beers.sh
@@ -7,4 +7,4 @@ curl -o /tmp/beers.zip https://storage.googleapis.com/slorello/beer_file.zip
 echo Unzipping Beers file
 unzip /tmp/beers.zip -d /tmp > /dev/null 2>&1
 echo Uploading beers file.
-$dir/upload.sh /tmp/beers limit 5
+$dir/upload.sh /tmp/beers limit 500
