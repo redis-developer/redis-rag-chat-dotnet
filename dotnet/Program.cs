b7288ef875e8ba113a58d6a21ad1830873b281d9
slorello89 <steve.lorello@redis.com>
2024-01-12 17:06:27 -0500

    program.cs cleanup

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,11 +1,4 @@
 using Microsoft.KernelMemory;
-using Microsoft.KernelMemory.AI;
-using Microsoft.KernelMemory.AI.OpenAI;
-using Microsoft.KernelMemory.Configuration;
-using Microsoft.KernelMemory.Handlers;
-using Microsoft.KernelMemory.MemoryDb.Redis;
-using Microsoft.KernelMemory.MemoryStorage;
-using Microsoft.KernelMemory.Search;
 using Microsoft.SemanticKernel;
 using Redis.OM;
 using Redis.OM.Contracts;
@@ -21,7 +14,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-builder.Services.AddOpenAITextEmbeddingGeneration(builder.Configuration["OpenAIEmbeddingGenerationModelId"]!, builder.Configuration["OpenAIApiKey"]!);
 var muxer = ConnectionMultiplexer.Connect("localhost");
 
 builder.Services.AddSingleton(new QueryConfiguration());
@@ -29,15 +21,6 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(muxer);
 builder.Services.AddSingleton<IRedisConnectionProvider>(new RedisConnectionProvider(muxer));
 builder.Services.AddSingleton<IChatMessageService>(
     sp => new ChatMessageService(sp.GetService<IRedisConnectionProvider>()!));
-ILogger<OpenAITextGenerator>? log = null;
-
-builder.Services.AddScoped<IMemoryDb>(sp  => 
-    new RedisMemory(
-        new RedisConfig(), 
-        sp.GetService<IConnectionMultiplexer>()!,
-        sp.GetService<ITextEmbeddingGenerator>()!)
-);
-builder.Services.AddScoped<ISearchClient>(sp => new SearchClient(sp.GetService<IMemoryDb>()!, sp.GetService<ITextGenerator>()!));
 
 var kernelBuilder = Kernel.CreateBuilder();
 
@@ -55,8 +38,6 @@ builder.Services.AddSingleton<ICompletionService>(sp=>
         sp.GetService<QueryConfiguration>()!,
         sp.GetService<Kernel>()!, 
         sp.GetService<IConfiguration>()!));
-builder.Services.AddScoped(sp => new MemoryService(null, sp.GetService<ISearchClient>()!));
-builder.Services.AddOpenAIChatCompletion(builder.Configuration["OpenAICompletionModelId"]!, builder.Configuration["OpenAIApiKey"]!);
 
 builder.Services.AddCors(options => options.AddDefaultPolicy(
     policy  =>
@@ -67,14 +48,9 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(
 var kmBuilder = new KernelMemoryBuilder();
 
 var memoryService = kmBuilder
-    // .WithOpenAITextGeneration(openAiConfig)
     .WithOpenAIDefaults(builder.Configuration["OpenAIApiKey"]!)
     .WithSimpleQueuesPipeline()
-    .With(new TextPartitioningOptions
-    {
-        MaxTokensPerLine = 255,
-        MaxTokensPerParagraph = 255
-    }).WithRedisMemoryDb(new RedisConfig(){ConnectionString = "localhost"})
+    .WithRedisMemoryDb(new RedisConfig(){ConnectionString = "localhost"})
     .Build<MemoryServerless>();
 
 builder.Services.AddSingleton<IKernelMemory>(memoryService);
@@ -92,7 +68,3 @@ app.UseHttpsRedirection();
 app.UseCors();
 app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
